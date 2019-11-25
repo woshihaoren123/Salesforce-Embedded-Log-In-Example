327dfe16aa37535115b6eff7580093a18e374a31
Dong, Zeming <zeming.dong@accenture.com>
2019-11-25 11:11:59 +0800

    Init

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var callbackRouter = require('./routes/callback');
-
 var app = express();
+
+const COMMUNITY_URL = process.env.COMMUNITY_URL;
+const APP_ID = process.env.APP_ID;
+const APP_SECRET = process.env.APP_SECRET;
+const OAUTH_CALLBACK_URL = process.env.OAUTH_CALLBACK_URL;
+const HOSTED_APP_URL = process.env.HOSTED_APP_URL;
+const BG_FAKE = process.env.BG_FAKE;
+const STATIC_ASSET_URL = process.env.STATIC_ASSET_URL;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,9 +24,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/** 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var callbackRouter = require('./routes/callback');
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/_callback', callbackRouter)
+app.use('/_callback', callbackRouter) */
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,6 +45,27 @@ app.use(function(err, req, res, next) {
     // render the error page
     res.status(err.status || 500);
     res.render('error');
+});
+
+//Routes
+app.get('/', function(req, res) {
+
+    res.render('./routes/index', {
+        community_url: COMMUNITY_URL,
+        app_id: APP_ID,
+        callback_url: OAUTH_CALLBACK_URL,
+        community_mode: COMMUNITY_MODE
+    })
+});
+
+app.get('/_callback', function(req, res) {
+
+    res.render('./routes/callback', {
+        community_url: COMMUNITY_URL,
+        app_id: APP_ID,
+        callback_url: OAUTH_CALLBACK_URL,
+        community_mode: COMMUNITY_MODE
+    })
 });
 
 module.exports = app;
